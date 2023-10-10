d3a163b209f1864ae55955bff67a14f07d3c4a18
Gezellighied <diepminhquan123@gmail.com>
2023-10-10 23:57:18 +0700

    Details Done

--- DOANPM_TH/DOANPM_TH/Models/ClassModel/Laptops.cs
+++ DOANPM_TH/DOANPM_TH/Models/ClassModel/Laptops.cs
@@ -5,9 +5,9 @@
         public int LaptopID { get; set; }
         public string LaptopName { get; set; }
         public string Brand { get; set; }
-        public int ScreenSize { get; set; }
+        public string ScreenSize { get; set; }
         public string Processor { get; set; }
-        public int Ram { get; set; }
+        public string Ram { get; set; }
         public int Price { get; set; }
         public string Image { get; set; }
     }
--- DOANPM_TH/DOANPM_TH/Models/ViewModel/LatopsViewModel.cs
+++ DOANPM_TH/DOANPM_TH/Models/ViewModel/LatopsViewModel.cs
@@ -5,9 +5,9 @@
         public int LaptopsID { get; set; }
         public string LaptopName { get; set; }
         public string Brand { get; set; }
-        public double ScreenSize { get; set; }
+        public string ScreenSize { get; set; }
         public string Processor { get; set; }
-        public int Ram { get; set; }
+        public string Ram { get; set; }
         public double Price { get; set; }
         public string Image { get; set; }
     }
